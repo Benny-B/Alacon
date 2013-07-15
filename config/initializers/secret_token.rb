e5cf348d9655929dcaa9cb4e65ff01dfd7ffa5dd
Mlekena <mlekena@skidmore.edu>
2013-07-15 10:01:01 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlaConnection::Application.config.secret_token = '3ddd14f6581ea6d38c5e70e403cb86b312b0046309d57a471ba95c038bda70ea6a38e87fb19fc554a9506af6104905e5a0f9b4bcc91eb6a553eb8dc35d1c368f'
