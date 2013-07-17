2df158c59a2562c042ec66c1903b050f47bd83ac
Mlekena <mlekena@skidmore.edu>
2013-07-17 10:18:01 +0200

    edit code readability

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,11 +6,8 @@ class UserController < ApplicationController
   def update
   	if request.xhr?
   		@user = User.find(current_user)
-  		if @user.update_attributes(params[:user])
-  		end
+  		@user.update_attributes(params[:user])
   		@user
-  	else
-  		
   	end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,:recoverable, :rememberable, :trackable, :validatable
          #:confirmable, :stretches => 20
 
-  attr_accessible :education,:latitude, :longitude, :first_name, :last_name, :email, :password, :password_confirmation
+  attr_accessible :education,:latitude, :longitude, :first_name, :last_name, :email, :password, 
+        :password_confirmation
+        
   validate :ala_confirm
 
 
