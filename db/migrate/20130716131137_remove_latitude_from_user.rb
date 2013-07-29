3c55611a8dbfeb39a6ba22a3aeac6b4254637d89
Ben Ouattara <zouattar@u.rochester.edu>
2013-07-29 16:16:27 +0200

    removed duplicated migrations files

--- db/migrate/20130716131137_remove_latitude_from_user.rb
+++
@@ -1,9 +0,0 @@
-class RemoveLatitudeFromUser < ActiveRecord::Migration
-  def up
-    remove_column :users, :latitude
-  end
-
-  def down
-    add_column :users, :latitude, :int
-  end
-end
--- db/migrate/20130716131218_remove_longitude_from_user.rb
+++
@@ -1,9 +0,0 @@
-class RemoveLongitudeFromUser < ActiveRecord::Migration
-  def up
-    remove_column :users, :longitude
-  end
-
-  def down
-    add_column :users, :longitude, :int
-  end
-end
