403742ee4f0aaadc7c340d2880e3c4459310fcc7
Ben Ouattara <zouattar@u.rochester.edu>
2013-07-30 17:17:50 +0200

    configured redis on heroku

--- nodeserver.js
+++ nodeserver.js
@@ -1,11 +1,22 @@
 var io = require('socket.io').listen(3332);
-var redis = require("redis").createClient();
+var redis;
+if(process.env.REDISTOGO_URL){
+var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+redis = require("redis").createClient(rtg.port, rtg.hostname);
+
+redis.auth(rtg.auth.split(":")[1]); 
+
+}else{
+	redis = require("redis").createClient();
+	console.log("redis created");
+}
 
 redis.subscribe('new post');
 
 io.sockets.on('connection', function(socket){
 	redis.on('message', function(channel, message){
 		socket.emit('new post', message);
+		console.log(message);
 	});
 });
 
