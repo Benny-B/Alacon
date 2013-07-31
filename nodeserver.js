18b04ad5c6bb622dbcb1f9c7ba3b320740a000b2
Ben Ouattara <zouattar@u.rochester.edu>
2013-07-31 10:38:35 +0200

    added back procfile

--- nodeserver.js
+++ nodeserver.js
@@ -1,9 +1,10 @@
 var io = require('socket.io').listen(process.env.PORT || 3332);
 var redis;
+
 if(process.env.REDISTOGO_URL){
 var rtg   = require("url").parse(process.env.REDISTOGO_URL);
 redis = require("redis").createClient(rtg.port, rtg.hostname);
-
+console.log("Redis client created");
 redis.auth(rtg.auth.split(":")[1]); 
 
 }else{
@@ -16,7 +17,7 @@ redis.subscribe('new post');
 io.sockets.on('connection', function(socket){
 	redis.on('message', function(channel, message){
 		socket.emit('new post', message);
-		console.log(message);
+		console.log("Message sent");
 	});
 });
 
